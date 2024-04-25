3d1945646c05ff5f5a6cb2929d21baf3df5285a1
on line lab <on line lab@DESKTOP-G6MG3J5>
2024-04-25 12:56:31 +0530

    push-1

---
+++ simple/src/simple/loginn.java
@@ -0,0 +1,97 @@
+package simple;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Color;
+import javax.swing.JButton;
+
+public class login {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JPasswordField passwordField;
+	private JButton btnNewButton;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					login window = new login();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public login() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.CYAN);
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("LOGIN PAGE");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 14));
+		lblNewLabel.setBounds(171, 11, 86, 14);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("USER");
+		lblNewLabel_1.setBounds(40, 57, 46, 14);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("PASSWORD");
+		lblNewLabel_2.setBounds(40, 127, 80, 17);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		textField = new JTextField();
+		textField.setBounds(171, 54, 86, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(171, 127, 86, 20);
+		frame.getContentPane().add(passwordField);
+		
+		btnNewButton = new JButton("SUBMIT");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String name=textField.getText();
+				String pwd=passwordField.getText();
+				if(name.equals("Neha")&&pwd.equals("1234")) {
+					JOptionPane.showMessageDialog(btnNewButton,"Valid user");
+				}
+				
+			else {
+				JOptionPane.showMessageDialog(btnNewButton,"InValid user");
+			}
+				
+			}
+			});;
+		btnNewButton.setBounds(94, 181, 89, 23);
+		frame.getContentPane().add(btnNewButton);
+
+	}
+}
